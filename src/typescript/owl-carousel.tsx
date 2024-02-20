88a4b532986b85f4cf6fec775b516f10b1122dfc
Lucasgdt <123172370+Lucasgdt@users.noreply.github.com>
2024-02-20 17:41:55 +0100

    update fruitetlegume

--- src/typescript/owl-carousel.tsx
+++ src/typescript/owl-carousel.tsx
@@ -1,23 +1,45 @@
-import React from 'react'
-import OwlCarousel from  'react-owl-carousel'
+import React, { useEffect, useRef } from 'react'
 import "../css/bootstrap.min.css"
 import "../css/style.css"
 import Product from "./product"
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
 
-function owlCarousel(){
+const OwlCarouselComponent: React.FC = () => {
+    const owlRef = useRef<HTMLDivElement>(null); // Référence vers l'élément racine du carrousel
 
+    useEffect(() => {
+        const initializeCarousel = () => {
+            if (owlRef.current) {
+                $(owlRef.current).owlCarousel({
+                    items: 3, // Nombre d'éléments à afficher
+                    loop: true, // Boucler le carrousel
+                    margin: 10, // Marge entre les éléments
+                    autoplay: true, // Activer l'autoplay
+                    autoplayTimeout: 3000, // Délai d'autoplay en millisecondes
+                });
+            }
+        };
 
-    return(
-        <div className="owl-carousel vegetable-carousel justify-content-center">
-            <Product/>
-            <Product/>
-            <Product/>
+        initializeCarousel(); // Appel de la fonction pour initialiser le carrousel
+
+        // Retour de la fonction de nettoyage pour détruire le carrousel lors du démontage du composant
+        return () => {
+            if (owlRef.current) {
+                $(owlRef.current).owlCarousel('destroy');
+            }
+        };
+    }, []);
+
+    return (
+        <div ref={owlRef} className="owl-carousel">
             <Product/>
             <Product/>
             <Product/>
             <Product/>
             <Product/>
         </div>
-    )
-}
-export default owlCarousel;
+    );
+};
+
+export default OwlCarouselComponent;
--- src/typescript/pagefruitslegumes.tsx
+++ src/typescript/pagefruitslegumes.tsx
@@ -2,14 +2,14 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import "../css/bootstrap.min.css"
 import "../css/style.css"
 import React from 'react';
-import OwlCarousel from "./owl-carousel";
+import OwlCarouselComponent from "./owl-carousel"
 
 function App(){
     return(
         <div className="container-fluid vesitable py-5">
             <div className="container py-5">
                 <h1 className="mb-0">Fruit et Légume - Hiver</h1>
-                <OwlCarousel/>
+                <OwlCarouselComponent/>
             </div>
         </div>
     );
